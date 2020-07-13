2839afc8b6f334b4897d70061ad38eaf30bda169
Salma Salah <salmasalahmahmoud@gmail.com>
2020-07-14 01:51:16 +0200

    adding outlets and studying notes

--- RollTheDice/RollTheDice/ViewController.swift
+++ RollTheDice/RollTheDice/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewOne.alpha = 1
+        
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
 
